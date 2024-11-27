6ebc2a645adbf00b946ae659ea84c1edb8cfb196
SPerez-23 <stevenpr@estudiantec.cr>
2024-11-27 02:10:31 -0600

    service para el login y register

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,16 @@
+import { AuthService } from './../../../cars/services/auth.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -14,25 +18,37 @@ export class LoginComponent {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required]],
       rememberMe: [false]
     });
   }
 
   onSubmit() {
-    // if (this.loginForm.valid) {
-    //   this.authService.login(this.loginForm.value).subscribe(
-    //     response => {
-    //       console.log('Login successful', response);
-    //     },
-    //     error => {
-    //       console.error('Login failed', error);
-    //     }
-    //   );
-    // }
+    this.authService.login(this.email, this.password).subscribe({
+      next: response => {
+        // Verificar el rol del usuario después del login
+        const userRole = response.role; // admin, user, logistics 
+        if (userRole === 'admin') {
+          this.router.navigate(['/homeAdmin']);
+        }
+        if (userRole === 'user') {
+          this.router.navigate(['/home']);
+        }
+        else {
+          this.router.navigate(['/homeLogistics']); 
+        }
+      },
+      error: error => {
+        this.errorMessage = 'Correo o contraseña incorrectos';
+      }
+    });
   }
 
 }
